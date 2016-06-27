8005e3b4c2c0d7d1c8173f742ff06bd3d4d3be1d
Arlen Burton and Inthrayuth Mahaphol <arlen+inthrayuth@>
2016-06-27 14:30:53 -0700

    Add ability to pass data between activities.

--- app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
+++ app/src/main/java/com/example/guest/myrestaurants/RestaurantsActivity.java
@@ -1,13 +1,20 @@
 package com.example.guest.myrestaurants;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class RestaurantsActivity extends AppCompatActivity {
+    private TextView mLocationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurants);
+        mLocationTextView = (TextView) findViewById(R.id.locationTextView);
+        Intent intent = getIntent();
+        String location = intent.getStringExtra("location");
+        mLocationTextView.98("Here are all the restaurant near: " + location);
     }
 }
